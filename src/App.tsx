8bc9c5ab445a8cbf3aa440aa15b1c13d8c798f66
jo-duchan <jo_duchan@icloud.com>
2022-11-10 00:09:41 +0900

    Pages 초기 설정

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PATH from "Path";
 import { GlobalStyle } from "styles/common";
 
+// Pages
+import Home from "pages/Home";
+import Test from "pages/Test";
+import Error from "pages/Error";
+
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyle />
       <Routes>
-        <Route path={PATH.Root} element={<>Home</>} />
-        <Route path={PATH.Test} element={<>Test</>} />
-        <Route path={PATH.Error} element={<>Page Not Found</>} />
+        <Route path={PATH.Root} element={<Home />} />
+        <Route path={PATH.Test} element={<Test />} />
+        <Route path={PATH.Error} element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Error.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+function Error() {
+  return <Container>Error</Container>;
+}
+
+export default Error;
+
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+`;
