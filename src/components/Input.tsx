943c86d01ad921adda77d1e024d224e03470bb10
jo-duchan <jo_duchan@icloud.com>
2022-11-20 22:09:47 +0900

    Create Input Components

---
+++ src/components/Input.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+function Input() {
+  return <Container> </Container>;
+}
+
+export default Input;
+
+const Container = styled.div``;
