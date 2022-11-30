04b75230f568945038a22fbf08109d87d43b8365
jo-duchan <jo_duchan@icloud.com>
2022-12-01 00:25:58 +0900

    Toggle Element 기본 스타일

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled, { css } from "styled-components";
+import ColorSystem from "styles/color-system";
 
 // Type
 import { Sizes } from "type/toggle-type";
@@ -22,14 +23,51 @@ Toggle.defaultProps = {
 };
 
 const Container = styled.div<StyledProps>`
+  position: relative;
+  background: ${ColorSystem.Neutral[400]};
+  border-radius: 100px;
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 50%;
+    transform: translate3d(0, -50%, 0);
+    background: ${ColorSystem.Neutral[0]};
+    border-radius: 100%;
+  }
   ${(props) => {
     switch (props.size) {
       case "SMALL":
-        return css``;
+        return css`
+          width: 36px;
+          height: 20px;
+
+          &::before {
+            left: 2px;
+            width: 16px;
+            height: 16px;
+          }
+        `;
       case "MEDIUM":
-        return css``;
+        return css`
+          width: 44px;
+          height: 24px;
+          &::before {
+            left: 3px;
+            width: 18px;
+            height: 18px;
+          }
+        `;
       case "LARGE":
-        return css``;
+        return css`
+          width: 60px;
+          height: 32px;
+          &::before {
+            left: 4px;
+            width: 24px;
+            height: 24px;
+          }
+        `;
       default:
         return css``;
     }
