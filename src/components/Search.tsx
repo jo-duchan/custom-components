3a29d295056693dca167df540477a4eaff9dbc70
jo-duchan <jo_duchan@icloud.com>
2022-12-03 00:58:30 +0900

    Create Search Components

---
+++ src/components/Search.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+function Search() {
+  return <Container> </Container>;
+}
+
+export default Search;
+
+const Container = styled.div``;
