6bdde12c40cdadff1ddca5625b3f41dd612778e0
jo-duchan <jo_duchan@icloud.com>
2022-12-10 01:28:44 +0900

    Textarea Components type states 추가

--- src/type/textarea-type.ts
+++ src/type/textarea-type.ts
@@ -1,6 +1,16 @@
+export type States =
+  | "DEFAULT"
+  | "HOVERED"
+  | "PRESSED"
+  | "FOCUSED"
+  | "LOADING"
+  | "DISABLED"
+  | "SKELETON";
+
 export type Sizes = "SMALL" | "MEDIUM" | "LARGE";
 
 export interface TextareaProps {
+  states?: States;
   size?: Sizes;
   label?: string;
   placeholder?: string;
