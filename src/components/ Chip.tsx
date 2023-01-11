9c501d2b9911424b4db639268e4ae64b77bea5f0
jo-duchan <jo_duchan@icloud.com>
2023-01-12 00:03:13 +0900

    Create Component Chip

---
+++ src/components/ Chip.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+function Chip() {
+    return (
+        <Container> </Container>
+    )
+};
+
+export default Chip;
+
+const Container = styled.div``;
