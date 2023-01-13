7583df28be64f5153fb3a5160f6cedc4236c8a2a
jo-duchan <jo_duchan@icloud.com>
2023-01-14 00:12:48 +0900

    Create PillTab.tsx

---
+++ src/components/PillTab.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+function PillTab() {
+  return <Container> </Container>;
+}
+
+export default PillTab;
+
+const Container = styled.div``;
