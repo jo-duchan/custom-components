8c9802967f5847aa050835c262354b774184a580
jo-duchan <jo_duchan@icloud.com>
2023-02-02 00:04:05 +0900

    Create ModalHeader Component

---
+++ src/components/ModalHeader.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+function ModalHeader() {
+  return <Container> </Container>;
+}
+
+export default ModalHeader;
+
+const Container = styled.div``;
