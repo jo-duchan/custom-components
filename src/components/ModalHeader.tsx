a8557ad767363b4938f3d45c9ba08fa5b8156224
jo-duchan <jo_duchan@icloud.com>
2023-02-03 00:22:26 +0900

    Modalheader 기본 스타일 구조

--- src/components/ModalHeader.tsx
+++ src/components/ModalHeader.tsx
@@ -2,9 +2,37 @@ import React from "react";
 import styled from "styled-components";
 
 function ModalHeader() {
-  return <Container> </Container>;
+  return (
+    <Container>
+      <Content> </Content>
+      <Overlay />
+    </Container>
+  );
 }
 
 export default ModalHeader;
 
-const Container = styled.div``;
+const Container = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`;
+
+const Content = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate3d(50%, 50%, 0);
+`;
+
+const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.6);
+  backdrop-filter: blur(10px);
+`;
