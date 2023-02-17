e72ab2cd28790b1133239698b63766c59caf5fae
jo-duchan <jo_duchan@icloud.com>
2023-02-18 00:11:51 +0900

    Create Component CopyText

---
+++ src/components/CopyText.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+function CopyText() {
+  return <Container> </Container>;
+}
+
+export default CopyText;
+
+const Container = styled.div``;
